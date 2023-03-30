171b7a7646c11596c799203d09c52746f3833514
BetterCallAle <dap22091998@gmail.com>
2023-03-30 15:24:54 +0200

    Ex completed

--- src/main/java/org/lessons/java/springlamiapizzeriacrud/controller/PizzaController.java
+++ src/main/java/org/lessons/java/springlamiapizzeriacrud/controller/PizzaController.java
@@ -3,13 +3,18 @@ package org.lessons.java.springlamiapizzeriacrud.controller;
 import org.lessons.java.springlamiapizzeriacrud.model.Pizza;
 import org.lessons.java.springlamiapizzeriacrud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/pizzas")
@@ -17,10 +22,28 @@ public class PizzaController {
     @Autowired
     private PizzaRepository pizzaRepo;
 
+    //INDEX
     @GetMapping
-    public String index(Model model){
-        List<Pizza> pizzas = pizzaRepo.findAll();
+    public String index(@RequestParam(name = "s") Optional<String> s, Model model){
+        List<Pizza> pizzas;
+
+        if(s.isEmpty()){
+            pizzas =  pizzaRepo.findAll();
+        } else {
+            pizzas = pizzaRepo.findByNameContainingIgnoreCase(s.get());
+            model.addAttribute("search", s.get());
+        }
+
         model.addAttribute("pizzas", pizzas);
+
         return "/pizzas/index";
+    }
+
+    //SHOW
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") Integer id, Model model){
+        Pizza pizza = pizzaRepo.findById(id).orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND));
+        model.addAttribute("pizza", pizza);
+        return "/pizzas/show";
     }
 }
--- src/main/java/org/lessons/java/springlamiapizzeriacrud/repository/PizzaRepository.java
+++ src/main/java/org/lessons/java/springlamiapizzeriacrud/repository/PizzaRepository.java
@@ -3,5 +3,9 @@ package org.lessons.java.springlamiapizzeriacrud.repository;
 import org.lessons.java.springlamiapizzeriacrud.model.Pizza;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
+    public List<Pizza> findByNameContainingIgnoreCase(String name);
 }
